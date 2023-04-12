c3b25c4d0995e1842a11c70358b2beeb4d02af86
Thomas <thomas.powis@maine.edu>
2023-04-12 10:33:08 -0400

    Merge branch 'main' of https://github.com/ThomasPowis/Lab10.git into main
    More setup

--- Lab10/src/HashTable.java
+++ Lab10/src/HashTable.java
@@ -2,10 +2,16 @@ import java.util.LinkedList;
 
 public class HashTable {
 
-	public static String[] hashTable = new String[500];
+	public static Student[] hashTable = new String[500];
 	
 	public static boolean add(String input) {
+		int ascii = hashCode(input);
+		Student tempStudent = new Student(input,ascii);
 		
+		//First take in input, 
+		//Then calculate hashcode
+		//Next create a student object of that hashCode
+		//Finally add it to the table
 	}
 	
 	public static boolean resize() {
@@ -19,6 +25,8 @@ public class HashTable {
 	
 	public static String hashCode(String input) {
 		//This is a double hashing function
+		//First calculate hash of first value
+		//Then search the hashTable for that value, if there is a collision probe the second value
 	}
 	
 	
