dde11427d44c68824d73c60c7acf0bc6466478fc
Thomas <thomas.powis@maine.edu>
2023-04-12 10:19:42 -0400

    Setting up hash table

--- Lab10/src/HashTable.java
+++ Lab10/src/HashTable.java
@@ -1,8 +1,29 @@
+import java.util.LinkedList;
 
 public class HashTable {
 
+	public static String[] hashTable = new String[500];
+	
+	public static boolean add(String input) {
+		
+	}
+	
+	public static boolean resize() {
+		
+	}
+	
+	public static String find(String input) {
+		
+	}
+	
+	
+	public static String hashCode(String input) {
+		//This is a double hashing function
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println();
 
 	}
 
